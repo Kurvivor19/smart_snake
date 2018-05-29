cb5548547ef623987cfcc8554977d0c31d6d4c3b
Ivan Truskov <trus19@gmail.com>
2018-05-29 18:12:30 +0300

    various refactoring

--- src/snake.c
+++ src/snake.c
@@ -2,46 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "node.h"
 #include "snake.h"
 
-struct map game;
-bool check_bite(struct snake_data* Pnake, int code_step);
-
-void born(struct snake_data* Psnake, int x, int y)
+void born(struct snake_data* psnake, int x, int y, int max_len)
 {
-	Psnake->length = 1;
-	//Psnake->head = make_node(x, y);
-	Psnake->dir = direc_stop;
+    psnake->length = 1;
+    psnake->dir = direc_stop;
+    psnake->buffer_size = 10 + max_len;
+    psnake->x = (int*)malloc(sizeof(int) * psnake->buffer_size);
+    psnake->y = (int*)malloc(sizeof(int) * psnake->buffer_size);
 }
 
-void unmake_nodes(struct node* p)
-{
-	struct node* temp;
-	if (p == 0)
-		return;
-	temp = p->tail;
-	free(p);
-	unmake_nodes(temp);
-}
+//void unmake_nodes(struct node* p)
+//{
+//	struct node* temp;
+//	if (p == 0)
+//		return;
+//	temp = p->tail;
+//	free(p);
+//	unmake_nodes(temp);
+//}
 
+//void insert_after(struct node* p, int x, int y)
+//{
+//	struct node* temp;
+//	temp = make_node(x, y);
+//	temp->tail = p->tail;
+//	p->tail = temp;
+//}
 
-void insert_after(struct node* p, int x, int y)
-{
-	struct node* temp;
-	temp = make_node(x, y);
-	temp->tail = p->tail;
-	p->tail = temp;
-}
-
-bool check_bite(struct snake_data* Psnake, int code_step) {
-	for (int num = 1; Psnake->length > num; num++)
-	{
-		if (game.snake.x[game.code_step - num] == game.snake.head_x) {
-			if (game.snake.y[game.code_step - num] == game.snake.head_y) {
-				return false;
-			}
-		}
-	}
-	return true;
+bool check_bite(struct snake_data* psnake, int code_step) {
+    for (int num = 1; psnake->length > num; num++)
+    {
+        if (psnake->x[(code_step % psnake->buffer_size) - num] == psnake->head_x) {
+            if (psnake->y[(code_step % psnake->buffer_size) - num] == psnake->head_y) {
+                return false;
+            }
+        }
+    }
+    return true;
 };
+
+void create_map(struct map* pmap, int w, int h)
+{
+    pmap->width = w;
+    pmap->height = h;
+    pmap->snake.head_x = rand() % w;
+    pmap->snake.head_y = rand() % h;
+    born(&(pmap->snake), pmap->snake.head_x, pmap->snake.head_y, w * h);
+    pmap->seed_x = rand() % w;
+    pmap->seed_y = rand() % h;
+}
